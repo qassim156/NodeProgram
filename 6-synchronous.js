6364d681fe4ca236b339241a276f974af9d7dc28
root <root@ADAKO>
2024-12-06 06:27:31 +0100

    Message

---
+++ 6-synchronous.js
@@ -0,0 +1,9 @@
+console.log("Qassim");
+console.time();
+for(let i = 0; i < 1000000; i++){
+	const hte = document.querySelector('h3');
+	hte.textContent = "qassin";
+}
+console.timeEnd();
+
+console.log("next task");
